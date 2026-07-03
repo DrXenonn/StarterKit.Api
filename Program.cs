9687c99dc23af6228d6de5767bc436385e31f801
Xenon <sina274621@gmail.com>
2026-07-03 23:03:52 +0330

    feat: add Serilog structured logging

--- Program.cs
+++ Program.cs
@@ -1,37 +1,63 @@
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
+using Serilog;
 using StarterKit.Api.Data;
 using StarterKit.Api.Endpoints;
 
-var builder = WebApplication.CreateBuilder(args);
+// Serilog
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.File(
+            "/var/log/starter.log",
+            rollingInterval: RollingInterval.Day,
+            retainedFileCountLimit: 7)
+    .CreateLogger();
 
-builder.Services.AddDbContextPool<AppDbContext>(opt =>
-        opt.UseNpgsql(builder.Configuration.GetConnectionString("Default"),
-            o => o
-            .UseNodaTime()));
-// o.MapEnum<Mood>("mood") later for using postgres enums.
-
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+try
 {
-    app.MapOpenApi();
-    app.MapScalarApiReference("/docs", options =>
-            options
-            .WithTitle("Template API")
-            .WithTheme(ScalarTheme.Saturn)
-            .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.Fetch)
-            );
-}
+    Log.Information("Starting web application");
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Services.AddSerilog();
 
-// For production, run "dotnet ef database update" separately instead.
-using (var scope = app.Services.CreateScope())
+    builder.Services.AddDbContextPool<AppDbContext>(opt =>
+            opt.UseNpgsql(builder.Configuration.GetConnectionString("Default"),
+                o => o
+                .UseNodaTime()));
+    // o.MapEnum<Mood>("mood") later for using postgres enums.
+
+    builder.Services.AddOpenApi();
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+        app.MapScalarApiReference("/docs", options =>
+                options
+                .WithTitle("Template API")
+                .WithTheme(ScalarTheme.Saturn)
+                .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.Fetch)
+                );
+    }
+
+    // For production, run "dotnet ef database update" separately instead.
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        db.Database.Migrate();
+    }
+
+    app.MapEndpoints();
+    app.Run();
+}
+catch (Exception ex)
 {
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
+    Log.Fatal(ex, "Application terminated unexpectedly");
 }
-
-app.MapEndpoints();
-app.Run();
+finally
+{
+    Log.CloseAndFlush();
+}
